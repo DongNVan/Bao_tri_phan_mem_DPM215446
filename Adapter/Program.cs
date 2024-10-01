e00954a8b084adc557d0aa7b08a1d2061041fa3e
Admin <Admin@MT0416>
2024-10-01 09:52:12 +0700

    Add project files.

---
+++ Adapter/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Adapter.NetOptimized;
+
+using static System.Console;
+
+/// <summary>
+/// Adapter Design Pattern
+/// </summary>
+public class Program
+{
+    public static void Main()
+    {
+        // Non-adapted chemical compound 
+        var unknown = new Compound();
+        unknown.Display();
+
+        // Adapted chemical compounds
+        var water = new RichCompound(Chemical.Water);
+        water.Display();
+
+        var benzene = new RichCompound(Chemical.Benzene);
+        benzene.Display();
+
+        var ethanol = new RichCompound(Chemical.Ethanol);
+        ethanol.Display();
+
+        // Wait for user
+        ReadKey();
+    }
+}
+
+public enum Chemical
+{
+    Water,
+    Benzene,
+    Ethanol
+}
+
+public enum State
+{
+    Boiling,
+    Melting
+}
+
